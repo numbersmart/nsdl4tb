a9cfdd611a506d0f64a49ee79c058403cd44062d
Eduardo Flores <eduardo@enelmargen.org>
2016-06-25 11:58:46 -0500

    delete builds

---
+++ R/modelodata.R
@@ -0,0 +1,22 @@
+empleados <- "ACEMPL00.DBF"
+empresa <- "ACEMPR00.DBF"
+familias <- "ACGRUP00.DBF"
+linea <- "ACLINE00.DBF"
+metal <- "ACMATE00.DBF"
+prov <- "ACPROV00.DBF"
+cat_movimiento <- "ACTMIN00.DBF"
+movimientos_log <- "ADDEAR00.DBF" # creo
+transacciones <- "ADMOCL00.DBF" # ventas
+movimientos_log2 <- "ADMOIN00.DBF" # inventarios
+notas_compra <- "ADMOPR00.DBF"
+notas_vta_sep <- "ADNVMA00.DBF"
+log_de_algo <- "ADNVSE00.DBF" # son pocas (como 2k)
+log_entradas_xnota_compra <- "ADRASE00.DBF"
+algo_nivel_notacompra <- "ADREAR00.DBF"
+bajas <- "ADRESE00.DBF"
+separados_parecen <- "ADSEPA00.DBF"
+status_separados <- "ADSESE00.DBF" # parece
+no_se_nivel_inv <- "ADTESE00.DBF" # suena a separados
+parece_log_tickets <- "ADTIMA00.DBF" # ventas, pero sin ticket unico!
+parece_tickets_log_inv <- "ADTISE00.DBF"
+traspasos <- "ADTREM00.DBF"
